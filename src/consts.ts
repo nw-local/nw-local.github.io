dc6c9c9a90b146e6649710942b20964deb3195a1
benny <22062601+benpetty@users.noreply.github.com>
2026-04-14 12:00:09 -0700

    feat: replace old content system with Sanity-powered layout
    Remove starter template content (Markdown blog posts, placeholder images,
    old components) and scaffold new layout system with Nav, Footer, AgeGate,
    and a base Layout component that pulls site settings from Sanity.

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Northwest Local Cannabis';
-export const SITE_DESCRIPTION = 'Washington State licensed cannabis producer and processor.';
