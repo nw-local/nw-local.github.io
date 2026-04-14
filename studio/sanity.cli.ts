7bea56a96518a4c04a51131c1eed71793d427807
benny <22062601+benpetty@users.noreply.github.com>
2026-04-14 13:26:25 -0700

    chore: add Sanity Studio deployment appId

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -6,10 +6,7 @@ export default defineCliConfig({
     dataset: 'production'
   },
   deployment: {
-    /**
-     * Enable auto-updates for studios.
-     * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
-     */
+    appId: 't5hs9tn8wrxxyihkxlo77eki',
     autoUpdates: true,
   }
 })
